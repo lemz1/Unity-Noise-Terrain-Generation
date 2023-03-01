4f93efb027f8c7752d98e7df15d95b9580a04c96
lemz1 <87707926+lemz1@users.noreply.github.com>
2023-03-01 22:23:10 +0100

    bruh

--- NoiseTerrainGeneration/Scripts/MeshGenerator.cs
+++ NoiseTerrainGeneration/Scripts/MeshGenerator.cs
@@ -17,7 +17,7 @@ public static class MeshGenerator
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
         float topLeftX = (width - 1) / -2f;
-        float topLeftZ = (height - 1) / 2f;
+        float topLeftZ = (height - 1) / -2f;
 
         MeshData meshData = new MeshData(width, height);
         int vertexIndex = 0;
